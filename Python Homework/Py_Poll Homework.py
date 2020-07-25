09b65b76149e1fbb3d06ad7d5c0640c37b2833ac
crissymlee <66131888+crissymlee@users.noreply.github.com>
2020-07-25 09:49:12 -0500

    Updated - still needs more

--- Python Homework/Py_Poll Homework.py
+++ Python Homework/Py_Poll Homework.py
@@ -1,31 +1,52 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
+# In[83]:
 
 
 import pandas as pd
 import os
 import csv
 
-
+#bring in csv
 file = os.path.join('..','PyBank','election_data.csv')
 file_df = pd.read_csv(file)
 file_df.head()
 
 
-# In[4]:
+# In[93]:
 
 
+#calculate how many lines there are - total votes
 total = file_df['Voter ID'].count()
 
+#list of candidates
+#candidate = file_df['Candidate'].unique()
 
-# In[7]:
+#How many votes each
+candidate = file_df.Candidate.value_counts()
+
+#Percentages
+percent = (file_df.Candidate.value_counts()/total).apply('{:.0%}'.format)
+percent_df = percent
+
+#group them
+#end= candidates.groupby('total').nunique
+
+#winner
+winner = candidate.max()
+
+
+# In[94]:
 
 
 print('Election Results')
 print('--------------------------')
 print(f"Total Votes: {total}")
+print('--------------------------')
+print(f"{percent}{candidate}")
+print('--------------------------')
+print(f"Winner: {winner}")
 print('--------------------------')
 
 
